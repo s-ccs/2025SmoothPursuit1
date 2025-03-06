d5adeada525922504f2be32f68772fba93da0c50
geigermartin <geigermartin@gmx.de>
2025-03-06 14:50:59 +0100

    started on the psychopy rebuild

--- experiment/psychopy_test.py
+++ experiment/psychopy_test.py
@@ -1,15 +1,25 @@
 from psychopy import visual, core
+from psychopy.data import TrialHandler, importConditions
 import numpy as np
+from os.path import join
 
-refresh_hz = 60
+def run_trial(trial_info):
+    # runs an individual trial; trial_info should be an iteration of TrialHandler
+    img_path = "/home/stimulus/projects/2025SmoothPursuit1/experiment/assets/face_AF-225_mask.png"
+    img = visual.ImageStim(win, img_path)
 
-win = visual.Window([1472, 920], fullscr=True, monitor=0)
-img_path = "C:/Users/jevri/Documents/GitHub/2025SmoothPursuit1/experiment/assets/face_AF-225_mask.png"
-img = visual.ImageStim(win, img_path)
+    x_coords = list(np.linspace(-1, 1, refresh_hz*2))
+    x_coords += x_coords[::-1]
+    x_coords *= 20
 
-x_coords = np.linspace(-1, 1, refresh_hz*2)
+    for x_coor in x_coords:
+        img.pos = (x_coor, 0)
+        img.draw()
+        win.flip()
 
-for x_coor in x_coords:
-    img.pos = (x_coor, 0)
-    img.draw()
-    win.flip()
+
+
+refresh_hz = 390
+asset_dir = "assets"
+win = visual.Window([1920, 1080], screen=1, fullscr=True, monitor="sec_mon")
+
